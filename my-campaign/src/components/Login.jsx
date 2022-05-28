5f62004ac940e4da40c00cd2b23e50ed113cb088
Luis porto <lmportoesq@gmail.com>
2022-05-28 15:36:50 -0500

    feac: add css styles with tailwind

--- my-campaign/src/components/Login.jsx
+++ my-campaign/src/components/Login.jsx
@@ -19,11 +19,11 @@ function Login() {
         });
     }
     return (
-        <div className='container mx-auto my-auto w-3/4'>
-            <form className='shadow-lg shadow-blue-900 border-red-300 w-[32rem] h-[30rem] mx-auto grid grid-col-1 gap-3 text-center p-5 rounded-lg'>
+        <div className='container mx-auto my-auto w-[30rem] h-[30rem] mt-20'>
+            <form className='shadow-lg shadow-blue-900 border-red-300 h-[20rem] mx-auto my-auto grid grid-col-1 gap-3 text-center p-5 rounded-lg'>
                 <h1 className='shadow-md '>Login</h1>
-                <input className='border rounded border-black p-1'name="email" type="email" placeholder="Email" onChange={handleChange}/>
-                <input className='border rounded border-black p-1' name="password" type="password" placeholder="Password" onChange={handleChange}/>
+                <input className='border rounded border-black p-1 h-12'name="email" type="email" placeholder="Email" onChange={handleChange}/>
+                <input className='border rounded border-black p-1 h-12' name="password" type="password" placeholder="Password" onChange={handleChange}/>
                 <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" disabled={handleValidate()}>Enviar</button>
             </form>
         </div>
